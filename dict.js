730f2f63712e0cbcb1feeaf5f07eaa436658e430
alexgordontlv <alexaybn@gmail.com>
2021-04-20 19:08:11 +0300

    maxDepth

---
+++ dict.js
@@ -0,0 +1,29 @@
+collection => object
+
+
+const object = [
+    {
+    html: 'button',
+    section: 'about',
+    lng: 'en',
+    text: 'About'
+},
+{
+    section: 'about',
+    lng: 'he',
+    text: 'לגבי'
+},
+{
+    section: 'about',
+    lng: 'en',
+    text: 'About'
+},
+]
+
+
+object.filter(obg=>obj.section == 'about')
+
+const func About = ({button}) =>{
+
+    <button >{button.text}</button>
+}
