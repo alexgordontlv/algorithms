385521a880c9ba70850d239536fb01870ca78a53
alexgordontlv <alexaybn@gmail.com>
2021-05-08 14:51:39 +0300

    dijkstra solved

--- dijkstra2.js
+++ dijkstra2.js
@@ -1,47 +1,66 @@
-class Edge {
-	constructor(weight) {
-		this.weight = weight;
+class Node {
+	constructor() {
+		this.distance = Number.MAX_SAFE_INTEGER;
+		this.edges = [];
 	}
 }
-const infinity = Number.MAX_SAFE_INTEGER;
+const indinity = Number.MAX_SAFE_INTEGER;
 class Graph {
 	constructor() {
 		this.nodes = {};
 	}
 
 	addEdge(src, dest, weight) {
+		if (src == dest) return;
 		if (!this.nodes[src]) {
-			this.nodes[src] = [{ dest: dest, weight }];
-			this.nodes[dest] = [{ dest: src, weight }];
+			this.nodes[src] = { dist: indinity, edges: [{ dest: dest, weight }] };
+			if (!this.nodes[dest]) {
+				this.nodes[dest] = { dist: indinity, edges: [{ dest: src, weight }] };
+			} else {
+				this.nodes[dest].edges = [...this.nodes[dest].edges, { dest: src, weight }];
+			}
 		} else if (!this.nodes[dest]) {
-			this.nodes[dest] = [{ dest: src, weight }];
-			this.nodes[src] = [...this.nodes[src], { dest: dest, weight }];
+			this.nodes[dest] = { dist: indinity, edges: [{ dest: src, weight }] };
+			this.nodes[src].edges = [...this.nodes[src].edges, { dest: dest, weight }];
 		} else {
-			this.nodes[src] = [...this.nodes[src], { dest: dest, weight }];
-			this.nodes[dest] = [...this.nodes[dest], { dest: src, weight }];
+			this.nodes[src].edges = [...this.nodes[src].edges, { dest: dest, weight }];
+			this.nodes[dest].edges = [...this.nodes[dest].edges, { dest: src, weight }];
 		}
 	}
 
 	printGraph() {
-		console.log(this.nodes);
+		for (let node in this.nodes) {
+			console.log(node, '=>', this.nodes[node]);
+		}
 	}
 }
 
 const dijkstra = (graph, startNode) => {
 	if (!(startNode in graph.nodes)) return 'no such node';
-	const Queue = [startNode];
+	graph.nodes[startNode].dist = 0;
+	const Queue = [graph.nodes[startNode]];
+	const visited = new Set();
 	while (Queue.length > 0) {
 		let currentNode = Queue.shift();
-		for (let i = 0; i < currentNode.length; i++) {
-			//	if()
+		visited.add(currentNode);
+		console.log(currentNode);
+		for (let i = 0; i < currentNode.edges.length; i++) {
+			let shoretsDistance = Math.min(currentNode.edges[i].weight + currentNode.dist, graph.nodes[currentNode.edges[i].dest].dist);
+			graph.nodes[currentNode.edges[i].dest].dist = shoretsDistance;
+			if (!visited.has(graph.nodes[currentNode.edges[i].dest])) Queue.push(graph.nodes[currentNode.edges[i].dest]);
 		}
 	}
 };
 
 const myGraph = new Graph(5);
-myGraph.addEdge('A', 'B', 2);
-myGraph.addEdge('A', 'C', 3);
-myGraph.addEdge('B', 'C', 4);
+myGraph.addEdge('A', 'B', 6);
+myGraph.addEdge('A', 'D', 1);
+myGraph.addEdge('D', 'B', 2);
+myGraph.addEdge('B', 'C', 5);
+myGraph.addEdge('D', 'E', 1);
+myGraph.addEdge('B', 'E', 2);
+myGraph.addEdge('E', 'C', 5);
 
+//myGraph.printGraph();
+dijkstra(myGraph, 'A');
 myGraph.printGraph();
-// console.log(dijkstra(myGraph, 'c'));
